b82c1827de7966158d2843d04db9f5ea7d640a49
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-10-26 21:37:32 +0700

    feat(goroutine): goroutine === lightweight

--- goroutine_test.go
+++ goroutine_test.go
@@ -6,8 +6,20 @@ import (
 	"time"
 )
 
+func DisplayNumber(number int) {
+	fmt.Println("Display:", number)
+}
+
 func RunGreetUhkti() {
 	fmt.Println("Assalamualaikum, Ukhti")
+}
+
+func TestManyGoroutine(t *testing.T) {
+	for i := 0; i < 100000; i++ {
+		go DisplayNumber(i)
+	}
+
+	time.Sleep(5 * time.Second)
 }
 
 func TestCreateGoroutine(t *testing.T) {
