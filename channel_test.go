2bc9ea66ac6f09e5af82c1bb060ae7cd54ecddfd
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-10-29 09:27:03 +0700

    default condition in select channel

--- channel_test.go
+++ channel_test.go
@@ -7,6 +7,35 @@ import (
 	"time"
 )
 
+func TestDefaultSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	var counter int = 0
+
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data from channel 1:", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data from channel 2:", data)
+			counter++
+		default:
+			fmt.Println("Waiting for data...")
+		}
+		if counter == 2 {
+			break
+		}
+	}
+}
+
 func TestSelectChannel(t *testing.T) {
 	channel1 := make(chan string)
 	channel2 := make(chan string)
