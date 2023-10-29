bbb3a8dbc9b3ab007e7a07fa326562c8319864e2
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-10-29 15:15:14 +0700

    securely use primitive data with sync/atomic without worrying about race conditions

---
+++ atomic_test.go
@@ -0,0 +1,29 @@
+package golanggoroutine
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+func TestAtomic(t *testing.T) {
+	var x int64 = 0
+	var group sync.WaitGroup
+
+	for i := 1; i <= 1000; i++ {
+		group.Add(1)
+
+		go func() {
+			for j := 1; j <= 100; j++ {
+				atomic.AddInt64(&x, 1)
+			}
+
+			group.Done()
+		}()
+	}
+
+	group.Wait()
+
+	fmt.Println("X value is", x)
+}
