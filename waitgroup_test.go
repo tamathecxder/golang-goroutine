81e21c723d0a33a563284e7e5ba3434f08ffaee3
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-10-29 11:06:01 +0700

    try using WaitGroup for better goroutine waiting compared to time.Sleep

---
+++ waitgroup_test.go
@@ -0,0 +1,30 @@
+package golanggoroutine
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func RunAsynchronous(group *sync.WaitGroup) {
+	defer group.Done()
+
+	group.Add(1)
+
+	fmt.Println("Test")
+
+	time.Sleep(1 * time.Second)
+}
+
+func TestWaitGroup(t *testing.T) {
+	group := &sync.WaitGroup{}
+
+	for i := 1; i <= 100; i++ {
+		go RunAsynchronous(group)
+	}
+
+	group.Wait()
+
+	fmt.Println("WaitGroup is done!")
+}
