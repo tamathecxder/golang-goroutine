35fb95973b6a7486b027d7cc732a25deddb54cc7
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-10-29 09:35:37 +0700

    case of race condition in goroutine

--- channel_test.go
+++ channel_test.go
@@ -7,6 +7,22 @@ import (
 	"time"
 )
 
+func TestRaceCondition(t *testing.T) {
+	x := 0
+
+	for i := 1; i <= 1000; i++ {
+		go func() {
+			for j := 1; j <= 100; j++ {
+				x = x + 1
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+
+	fmt.Println("X value is", x)
+}
+
 func TestDefaultSelectChannel(t *testing.T) {
 	channel1 := make(chan string)
 	channel2 := make(chan string)
